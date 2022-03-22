870164956d455a584fe6adeb7e17cb4af673a1fd
zcKO <studyjchub@gmail.com>
2022-03-22 10:14:56 +0900

    채팅 UI 설정  - DB연결 정보를 BaseActivity 에서 상속

--- app/src/main/java/com/jc/realtimedatabase/BaseActivity.kt
+++ app/src/main/java/com/jc/realtimedatabase/BaseActivity.kt
@@ -3,10 +3,14 @@ package com.jc.realtimedatabase
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 abstract class BaseActivity: AppCompatActivity() {
 
     lateinit var mContext: Context
+
+    // 멤버 변수로 RealtimeDB 에 연결
+    val realtimeDB = FirebaseDatabase.getInstance("https://realtimedatabase-59b6c-default-rtdb.asia-southeast1.firebasedatabase.app/")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
