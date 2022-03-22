c641c6efc37d052cc05748d8421689e0a76ec705
zcKO <studyjchub@gmail.com>
2022-03-22 10:20:14 +0900

    realtimeDb 에, 하위 항목 구조 설정 = 데이터 기록 체험

--- app/src/main/java/com/jc/realtimedatabase/MainActivity.kt
+++ app/src/main/java/com/jc/realtimedatabase/MainActivity.kt
@@ -2,6 +2,9 @@ package com.jc.realtimedatabase
 
 import android.os.Bundle
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,22 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setEvents() {
+
+        btnSend.setOnClickListener {
+
+            val inputContent = edtContent.text.toString()
+
+            // 임시 : DB 의 하위 항목으로 => message 항목 => 0번 항목의 => content 항목 : 입력 내용
+            realtimeDB.getReference("message").child("0").child("content").setValue(inputContent)
+
+            // 추가 기록 : 현재시간 값을 "2022년 3월 5일 오후 5:05" 양식으로 기록.
+            val now = Calendar.getInstance()
+            val sdf = SimpleDateFormat("yyyy년 M월 d일 a h:mm")
+            val nowStr = sdf.format(now.time)
+
+            realtimeDB.getReference("message").child("0").child("createdAt").setValue(nowStr)
+
+        }
 
     }
 
